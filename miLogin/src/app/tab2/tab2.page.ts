5524925603fcebd3fde8639c17d10ac91221b5db
Jesus Alfonso Valenzuela Lagarda <jesualfonsovallagarda@gmail.com>
2021-04-18 14:59:38 -0700

    El """""""CRUD""""""" del brindis

--- miLogin/src/app/tab2/tab2.page.ts
+++ miLogin/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { producto } from './producto'
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  url="http://apiproductos.somee.com/api/productos";
+  clave:string;
+  nombre:string;
+  precio:number;
+  productos:producto[];
+  pro:producto;
 
-  constructor() {}
+  constructor(private http:HttpClient) {
+    this.llenarGrid();
+  }
+
+  buscar(){
+    this.http.get<producto>(this.url+"/"+this.clave).subscribe(resp=>{
+      this.pro=resp;
+    })
+
+  }
+  llenarGrid(){
+    this.http.get<producto[]>(this.url).subscribe(resp=>{
+      this.productos=resp;
+    })
+  }
+  guardar(){
+    let miProducto={
+      Pro_Cve:this.clave,
+      Pro_Nombre:this.nombre,
+      Pro_Precio:this.precio
+    }
+    this.http.post(this.url, miProducto).subscribe(
+      data=>{
+        console.log(data['_body']);
+      }, error=> {
+        console.log(error);
+      }
+    );
+    this.llenarGrid();
+    this.llenarGrid();
+  }
+  limpiar(){
+    this.clave="";
+    this.nombre="";
+    this.precio=0;
+    this.llenarGrid();
+  }
+  borrar(){
+    this.http.delete(this.url+"/"+this.clave).subscribe(data=>{
+      console.log
+    })
+  }
 
 }
